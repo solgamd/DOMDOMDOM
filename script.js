75529031b8c0a14b77bc8139880a96db74544e36
solgamd <solgamd@gmail.com>
2019-04-04 20:40:26 +0200

    Added button event listener; working on hover event listener

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     let button = document.createElement('button');
     let buttonText = document.createTextNode('Add Square');
     let container = document.createElement('div');
     button.appendChild(buttonText);
     container.className = 'container';
-    document.body.appendChild(button); 
-
-    button.addEventListener('click', addSquare);
-    
-    let boxCount = 0;
-
+    document.body.appendChild(button);
     document.body.appendChild(container);
 
-    function addSquare() {
-        console.log("worked");
+    let boxCount = 0;
 
+    button.addEventListener('click', addSquare);
+
+    function addSquare() {
         let box = document.createElement('div');
         box.className = 'box';
-        box.id = boxCount;
-        box.innerText = boxCount++;
-    
+        //box.innerText = boxCount++;
+
         container.appendChild(box);
-        
-    }; 
     
+        let boxText = boxCount;
+
+        box.addEventListener('mouseover', appear);
+
+        function appear() {
+            box.appendChild(boxText);
+        }
+
+        box.addEventListener('mouseout', disappear);
+
+        function disappear() {
+            box.remove(boxText);
+        }
+
+    };
+
+
+
+    
+
+
+
+
 })
